2d9841e6472ed59892feb8b65d70d79af74f71b0
Vicg853 <vicg853@gmail.com>
2022-01-15 19:11:35 -0300

    Modifying _documents

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,13 @@ class MyDocument extends Document {
     const page = await renderPage()
     // Extrach the css for each page render
     const css = extractCss()
-    return { ...page, css }
+    return { ...page, 
+      style: (
+        <style id='_goober'>
+          {css}
+        </style>
+      )
+    }
   }
   
   render() {
@@ -15,11 +21,6 @@ class MyDocument extends Document {
       <Html lang='pt'>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;400;500;800&family=Montserrat:wght@100;200;300;400;500;600;800&display=swap" rel="stylesheet" />
-          <style
-            id={'_goober'}
-            // And defined it in here
-            dangerouslySetInnerHTML={{ __html: ' ' + this.props.css }}
-          />
         </Head>
         <body>
           <Main />
